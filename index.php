72335bc24e70f8fc011309b8b7fa362795be84e1
Rysddd <rasyidpintar89@gmail.com>
2025-01-07 09:27:04 +0800

    selesai

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
+// simpan nama dan nisn kalian ke dalam sebuah variabel
+    $nama = "Muhammad Rasyid Shiddiq";
+    $nisn = "0071225807";
+// tampilkan atau cetak variabel ke dalama layar
+    echo 'Nama : ' ,$nama;
+    echo 'NISN : ' ,$nisn;
 
 ?>
